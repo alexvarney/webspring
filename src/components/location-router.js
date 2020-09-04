f83a3576c5a42ff03b050f5c3e2077949349fa53
seanclin <60147511+seanclin@users.noreply.github.com>
2020-09-04 14:36:30 -0400

    Add ValuesWall

--- src/components/location-router.js
+++ src/components/location-router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch, Link } from "react-router-dom";
 import ExampleLocation from "./location-screens/example";
 import KioskScreen from "./location-screens/KioskScreen";
+import ValuesWall from "./location-screens/ValuesWall";
 
 export default function LocationRouter() {
   return (
@@ -12,6 +13,9 @@ export default function LocationRouter() {
       <Route exact path="/2">
         You are at the 2nd path
         <Link to="/">Page 1</Link>
+      </Route>
+      <Route exact path="/values_wall">
+        <ValuesWall />
       </Route>
     </Switch>
   );
---
+++ src/components/location-screens/ValuesWall.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import InteractiveImage from "../interactive-image";
+import { Link, useHistory } from "react-router-dom";
+import { StateContext, ActionTypes } from "../../hooks/useApplicationState";
+import styled from "styled-components";
+
+const Image = styled.img`
+  width: 700px;
+`
+
+export default function ValuesWall() {
+  const history = useHistory();
+  const { state, dispatch } = useContext(StateContext);
+
+  return (
+    <Image
+      src="./office/values_wall.png"
+    >
+    </Image>
+  );
+}

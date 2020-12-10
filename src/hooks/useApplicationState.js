e02ae0af17c681c616e0343b0237c9da1632b2ee
Alexander Varney <alex@varney.me>
2020-12-10 17:38:58 -0500

    typo

--- src/hooks/useApplicationState.js
+++ src/hooks/useApplicationState.js
@@ -6,7 +6,7 @@ export const ActionTypes = {
   completePuzzle: "COMPLETE_PUZZLE",
 };
 
-//States: ['HIDDEN', 'MAP', 'LOCKSCREEN', 'LOCKSCREEN.UNLOCK', 'HOME', 'PLAYER', 'TINDOG']
+//States: ['HIDDEN', 'MAP', 'LOCKSCREEN', 'LOCKSCREEN.UNLOCK', 'HOME', 'PLAYER', 'TINDAWG']
 
 export const StateContext = React.createContext();
 

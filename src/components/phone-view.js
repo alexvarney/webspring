51cfbfdb63b683dfc5c8d5c45239b15d02f1c839
Alexander Varney <alex@varney.me>
2020-12-10 20:26:25 -0500

    cleanup phone-view

--- src/components/phone-view.js
+++ src/components/phone-view.js
@@ -1,11 +1,12 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import StatusBar from "./ui/statusbar";
-import { motion, useAnimation } from "framer-motion";
 import { ImPhone, ImPhoneHangUp } from "react-icons/im";
-import { RiDeleteBack2Line } from "react-icons/ri"
+import StateContainer from "./ui/state-container";
+import Sound from "react-sound";
+import { useStopwatch } from "react-timer-hook";
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled(StateContainer)`
   width: 100%;
   height: 100%;
   background-color: white;
@@ -14,35 +15,6 @@ const Wrapper = styled(motion.div)`
   color: black;
   opacity: 0;
   position: relative;
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  max-width: 100%;
-  align-items: center;
-  position: relative;
-  max-width: 100%;
-  min-height: 0;
-  overflow: hidden;
-  cursor: pointer;
-  padding-top: 35px;
-
-  & > img {
-    overflow: hidden;
-    object-fit: contain;
-    width: 5vh;
-    border-radius: 50%;
-    max-width: 50px;
-    max-height: 50px;
-  }
-
-`;
-
-const CancelButton = styled.img`
-
-  margin-left: 19px;
-
 `;
 
 const PasscodeInputWrapper = styled.div`
@@ -116,161 +88,165 @@ const PhoneInputView = styled.div`
   justify-content: center;
 `;
 
-function sound(src, loop = false) {
-  this.sound = document.createElement("audio");
-  this.sound.src = src;
-  this.sound.setAttribute("preload", "auto");
-  this.sound.setAttribute("controls", "none");
-  this.sound.style.display = "none";
-  this.sound.loop = loop
-  document.body.appendChild(this.sound);
-  this.play = function(){
-    this.sound.play();
-  }
-  this.stop = function(){
-    this.sound.pause();
-  }
-}
-
-var phoneTimeout;
-var phoneInterval;
-const phoneDial = new sound("./audio/phone_dial.mp3");
-const morseCode = new sound("./audio/morse_code.wav", true);
-var timer = 0;
-
 export default function PhoneView(props) {
   const [inputValue, setInputValue] = useState("");
-  const [callStatus, setCallStatus] = useState("");
-  const [isCalling, setIsCalling] = useState(false)
-  const [callSucceeded, setCallSucceeded] = useState(false)
+  const [callState, setCallState] = useState("INPUT"); // INPUT, DIALING, TRANSITION, FAILURE, SUCCESS
+  const isCalling = callState !== "INPUT";
+  const { seconds, start, reset } = useStopwatch();
+  const formattedTime = seconds < 10 ? `00:0${seconds}` : `00:${seconds}`;
 
-  React.useEffect(() => {
-    return () => {
-      hangUp();
-    };
-  }, [props.current]);
+  useEffect(() => {
+    switch (callState) {
+      case "SUCCESS":
+      case "FAILURE":
+        reset();
+        start();
+        break;
+      case "TRANSITION":
+        if (inputValue === "519-745-2222") {
+          setCallState("SUCCESS");
+        } else {
+          setCallState("FAILURE");
+        }
+        break;
+      default:
+        break;
+    }
+  }, [callState]);
 
   const handlePhoneInput = (event, char) => {
     event.stopPropagation();
     event.preventDefault();
 
-    setCallStatus("")
     setInputValue((prevVal) => {
-
-      if (prevVal.length == 12) {
+      if (prevVal.length === 12) {
         return prevVal;
       }
 
-      if (prevVal.length == 3 || prevVal.length == 7) {
-        prevVal += "-"
+      if (prevVal.length === 3 || prevVal.length === 7) {
+        prevVal += "-";
       }
-      console.log(char)
-      return prevVal + char.toString();
 
+      return prevVal + char.toString();
     });
   };
 
   const deletePhoneInput = () => {
-    setCallStatus("")
     setInputValue((prevVal) => {
-      return prevVal.slice(0, -1)
+      return prevVal.slice(0, -1);
     });
-  }
-
-
-  const successfulCall = () => {
-    setCallSucceeded(true)
-    morseCode.play();
-    incrementTimer()
-    phoneInterval = setInterval(incrementTimer, 1000)
-  }
-
-  const incrementTimer = () => {
-
-    var minutes = Math.floor(timer/60)
-    var seconds = timer%60
-    var minutesString = ""
-    var secondsString = ""
-    if (minutes < 10) {
-        minutesString = "0"
-    }
-    minutesString += minutes
-
-    if (seconds < 10) {
-      secondsString = "0"
-    }
-    secondsString += seconds
-
-    setCallStatus(minutesString + ":" + secondsString)
-    timer +=1
-  }
+  };
 
   const hangUp = () => {
-    setCallSucceeded(false)
-    setIsCalling(false)
-    clearInterval(phoneInterval)
-    clearTimeout(phoneTimeout)
-    setCallStatus("Call ended")
-    timer = 0;
-    //stop all audio files
-    phoneDial.stop()
-    morseCode.stop()
-    console.log("hanging up")
-  }
+    setCallState("INPUT");
+  };
 
   const call = () => {
-    phoneDial.stop();
-    phoneDial.play();
-    clearTimeout(phoneTimeout)
-    setCallStatus("Calling...")
-    setIsCalling(true)
+    setCallState("DIALING");
+  };
 
-    phoneTimeout = setTimeout(function() {
-      phoneDial.stop();
-      if (inputValue == "519-745-2222") {
-        //number called
-        successfulCall()
-      } else {
-        setCallStatus("Call failed")
-        setIsCalling(false)
-      }
-    }, 6000)
-  }
+  const phoneInputs = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "*",
+    "0",
+    "#",
+  ];
 
-  const phoneInputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#']
   return (
-    <Wrapper
-      initial={{ scale: 0, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      exit={{ scale: 0, opacity: 0 }}
-      key="map-container"
-    >
-      <StatusBar />
+    <>
+      <Sound
+        url="/audio/phone_dialing.mp3"
+        playStatus={callState === "DIALING" ? "PLAYING" : "STOPPED"}
+        onFinishedPlaying={() => setCallState("TRANSITION")}
+      />
+      <Sound
+        url="/audio/phone_fail.mp3"
+        playStatus={callState === "FAILURE" ? "PLAYING" : "STOPPED"}
+        onFinishedPlaying={() => setCallState("INPUT")}
+      />
+      <Sound
+        url="/audio/morse_code.wav"
+        playStatus={callState === "SUCCESS" ? "PLAYING" : "STOPPED"}
+        onFinishedPlaying={() => setCallState("INPUT")}
+      />
+      <Wrapper key="phone-container">
+        <StatusBar />
 
-      <PhoneInputView>
-        <center>{inputValue}</center>
-        <center>{callStatus}</center>
-      </PhoneInputView>
+        <PhoneInputView>
+          <p
+            style={{
+              textAlign: "center",
+              fontSize: "24px",
+              fontWeight: "700",
+            }}
+          >
+            {inputValue}
+          </p>
 
+          <p style={{ textAlign: "center", marginTop: "8px" }}>
+            {callState === "DIALING"
+              ? "Connecting..."
+              : callState !== "INPUT"
+              ? formattedTime
+              : ""}
+          </p>
+        </PhoneInputView>
 
-      <PasscodeInputWrapper>
-        <div
-          style={{
-            gridRow: "1",
-            gridColumn: "1",
-            width: 0,
-            paddingBottom: "100%",
-          }}
-        />
-        {phoneInputs.map((e, i) => (
-          <button key={i} onClick={(event)=>{handlePhoneInput(event, phoneInputs[i])}}>{phoneInputs[i]}</button>
-        ))}
-        <button className="clearButton"></button>
-        {!isCalling&&<button className="phoneIcon" onClick={()=>{call()}}><ImPhone color="white" /></button>}
-        {isCalling&&<button className="hangUpIcon" onClick={()=>{hangUp()}}><ImPhoneHangUp color="white" /></button>}
-        <button className="deleteButton" onClick={()=>{deletePhoneInput()}}></button>
-      </PasscodeInputWrapper>
-
-    </Wrapper>
-    );
+        <PasscodeInputWrapper>
+          <div
+            style={{
+              gridRow: "1",
+              gridColumn: "1",
+              width: 0,
+              paddingBottom: "100%",
+            }}
+          />
+          {phoneInputs.map((e, i) => (
+            <button
+              key={i}
+              onClick={(event) => {
+                handlePhoneInput(event, phoneInputs[i]);
+              }}
+            >
+              {phoneInputs[i]}
+            </button>
+          ))}
+          <button className="clearButton"></button>
+          {callState === "INPUT" ? (
+            <button
+              className="phoneIcon"
+              onClick={() => {
+                call();
+              }}
+            >
+              <ImPhone color="white" />
+            </button>
+          ) : (
+            <button
+              className="hangUpIcon"
+              onClick={() => {
+                hangUp();
+              }}
+            >
+              <ImPhoneHangUp color="white" />
+            </button>
+          )}
+          <button
+            className="deleteButton"
+            onClick={() => {
+              deletePhoneInput();
+            }}
+          ></button>
+        </PasscodeInputWrapper>
+      </Wrapper>
+    </>
+  );
 }

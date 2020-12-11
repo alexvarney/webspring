6a34e8839c7f9b20072e34e17577ab3cd0240507
seanclin <60147511+seanclin@users.noreply.github.com>
2020-12-11 14:52:26 -0500

    Add magnifier

--- src/components/panels/BookCase.js
+++ src/components/panels/BookCase.js
@@ -3,6 +3,7 @@ import InteractiveImage from "../shared/interactive-image";
 import { Link, useHistory } from "react-router-dom";
 import { StateContext, ActionTypes } from "../util/useApplicationState";
 import styled from "styled-components";
+import Magnifier from 'react-magnifier'
 
 const Image = styled.img`
   width: 700px;
@@ -11,6 +12,8 @@ const Image = styled.img`
 export default function BookCase() {
   const history = useHistory();
   const { state, dispatch } = useContext(StateContext);
-
-  return <Image src="./office/bookcase.png"></Image>;
+  return <Magnifier
+          src="./office/bookcase.jpg"
+          zoomFactor={3}
+          width={700} />;
 }

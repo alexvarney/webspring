0eb4b9de4b5a92d44f6db85e8f8ac4bcf37264aa
Alexander Varney <alex@varney.me>
2020-12-11 19:03:20 -0500

    add escape hatch for slack puzzle

--- src/components/apps/slack.js
+++ src/components/apps/slack.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import StatusBar from "../shared/statusbar";
 import StateContainer from "../shared/state-container";
@@ -18,7 +18,21 @@ const StyledStatusBar = styled(StatusBar)`
   position: absolute;
   top: 0;
 `;
+
+const hints = [
+  "I came up with adding those lat/lon lines, find me first on the map.",
+  "Some people don't like my projections, find me second on the map.",
+  "I have many interests, including maps, find me third on the map.",
+  "I'm the father of GIS, find me fourth on the map.",
+  "I take a social approach to geography, find me fifth on the map.",
+  "I created the modern atlas, find me sixth on the map",
+];
+
 export default function SlackView(props) {
+  useEffect(() => {
+    console.log(hints);
+  }, []);
+
   return (
     <Container
       onClick={() =>

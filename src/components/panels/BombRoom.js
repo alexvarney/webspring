0c1881d8bc2201c55840b390ae23b7edc8a2b58e
raghav-mappedin <72038919+raghav-mappedin@users.noreply.github.com>
2020-12-15 08:33:42 -0800

    win screen

--- src/components/panels/BombRoom.js
+++ src/components/panels/BombRoom.js
@@ -6,7 +6,18 @@ import styled from "styled-components";
 import { useStopwatch, useTimer } from "react-timer-hook";
 
 const Container = styled(InteractiveImageContainer)`
-  background-image: url("/office/bombdefusalUI.jpg");
+  background-image: url(${(props) => props.bgImage});
+`;
+
+const Congrats = styled.div`
+  position: absolute;
+  left: 30%;
+  right: 40%;
+  top: 15%;
+  bottom: auto;
+  
+  font-size: 50px;
+  color: pink;
 `;
 
 const TimerWrapper = styled.div`
@@ -62,6 +73,10 @@ export default function BombRoom() {
     });
   };
 
+  const [puzzleImage, setPuzzleImage] = useState(
+    "/office/bombdefusalUI.jpg"
+  );
+
   useEffect(() => {
     const isValid = dataInput.every(
       (value, index) => unlockCode[index].toLowerCase() === value.toLowerCase()
@@ -69,7 +84,8 @@ export default function BombRoom() {
 
     if (isValid) {
       timeoutRef.current = setTimeout(() => {
-        alert("Puzzle unlocked");
+        setPuzzleImage("https://media.giphy.com/media/cReBRwdnlW9gs/giphy.gif")
+        //alert("Puzzle unlocked");
       }, 250);
     }
   }, [dataInput]);
@@ -78,19 +94,6 @@ export default function BombRoom() {
     () => () => timeoutRef.current && clearTimeout(timeoutRef.current),
     []
   );
-
-  // STOPWATCH
-  // const {
-  //   seconds,
-  //   minutes,
-  //   hours,
-  //   days,
-  //   isRunning,
-  //   start,
-  //   pause,
-  //   reset,
-  // } = useStopwatch({ autoStart: true });
-  // STOPWATCH END
 
   const time = new Date();
   time.setSeconds(time.getSeconds() + 3600); // 10 minutes timer
@@ -110,13 +113,8 @@ export default function BombRoom() {
     onExpire: () => console.warn("onExpire called"),
   });
 
-  const formattedTime =
-    seconds < 10
-      ? `${hours}:${minutes}:0${seconds}`
-      : `${hours}:${minutes}:${seconds}`;
-
-  return (
-    <Container>
+  return (puzzleImage == "/office/bombdefusalUI.jpg" ? (
+    <Container bgImage={puzzleImage}>
       <TimerWrapper>
         <input type="text" value={hours} />
         <input type="text" value={minutes} />
@@ -146,5 +144,9 @@ export default function BombRoom() {
         />
       </InputWrapper>
     </Container>
-  );
+  ) : (
+      <Container bgImage={puzzleImage}>
+        <Congrats> Congratulations!  </Congrats>
+    </Container>
+  ));
 }
